28e41b824a865b1789d5889914c8cbf11bfb0d1a
Yamz <70824779+SuperRavenSn1per@users.noreply.github.com>
2024-05-05 09:59:21 -0400

    Update api.lua
    Created the "split" function

--- api.lua
+++ api.lua
@@ -3,3 +3,9 @@ function centerWrite(str, yPos)
   term.setCursorPos(w/2-string.len(str)/2,yPos or h/2)
   write(str)
 end
+
+function split(str, delim)
+  local sp = {}
+  for s in str:gmatch("([^"..delim.."]+)") do table.insert(sp, s) end
+  return sp
+end
